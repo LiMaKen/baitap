37a90952ca3dae978494fe4381ae6740884a64b9
hovetu <d.quangtu302@gmail.com>
2023-05-17 18:15:25 +0700

    .

--- js/main.js
+++ js/main.js
@@ -1,47 +1,66 @@
-const apiUser = 'http://localhost:3000/user';
+const apiUser = 'http://localhost:3000/user'
+
 function start (){
-  var onlickLogin = document.querySelector('.onclick2') 
-  var onclickRegister = document.querySelector('.onclick2') 
-  onlickLogin.onclick= () => login(event)
-
-  onclickRegister.onclick = () => register(event)
-
+  var onclickLogin = document.getElementById('onclick1') 
+  var onclickRegister = document.getElementById('onclick2') 
+  singupbutton()
+  onclickLogin.onclick = () =>  login()
+  onclickRegister.onclick = () => register()
+ 
 }
 start()
 
-function login(event) {
-  event.preventDefault();
+function login() {
 
-  const username = document.getElementById('loginUsername').value;
-  const password = document.getElementById('loginPassword').value;
+  const username = document.querySelector('.name__dangnhap').value
+  const password = document.querySelector('.pass__dangnhap').value
 
+  if ( username==='' || password ==='' )
+        {
+             alert(' mời nhập vào đầy đủ các ô trống')
+        }
+  else {
   fetch(apiUser)
     .then(response => response.json())
-    .then(users => {
-      const user = users.find(user => user.username === username && user.password === password);
-     console.log(users)
+    .then(users => 
+        {
+      const user = users.find(user => user.username === username && user.password === password)
+        console.log(users)
 
-      if (user) {
-        alert('Đăng nhập thành công!');
-        
-      } else {
-        alert('Sai ten dang nhập hoặc tài khoản');
-      }
+      if (user) 
+        {
+            alert('Đăng nhập thành công!')
+            window.location.href = "https://github.com/hovetu/baitap"
+        } 
+      
+      else 
+        {
+            alert('Sai ten dang nhập hoặc tài khoản')
+        }
     })
-    .catch(error => console.log('Lỗi:', error));
+    .catch(error => console.log('Lỗi:', error))
+}
 }
 
-function register(event) {
-  event.preventDefault();
+function register() {
 
-  const username = document.getElementById('registerUsername').value;
-  const password = document.getElementById('registerPassword').value;
+  const username = document.querySelector('.name__dangky').value;
+  const password = document.querySelector('.pass__dangky').value;
 
   const newUser = {
     username: username,
     password: password
-  };
-
+  }
+if (username ==='' || password==='')
+  {
+    alert('Mời bạn nhập vào đầy đủ các ô trống')
+  }
+else if (username.length >10 || password.length > 10)
+  {
+    alert('Tên tài khoản hoặc mật khẩu vượt quá ký tự cho phép')
+  }
+else
+    {
   fetch(apiUser, {
     method: 'POST',
     headers: {
@@ -51,8 +70,27 @@ function register(event) {
   })
     .then(response => response.json())
     .then(data => {
-      alert('Đăng ký thành công!');
-      // Thực hiện các hành động khác sau khi đăng ký thành công
+
+      alert('Đăng ký thành công!')
+      singupbutton()
     })
-    .catch(error => console.log('Lỗi đăng ký:', error));
+    .catch(error => console.log('Lỗi đăng ký:', error))
+  }
 }
+function singupbutton()
+    {
+const signUpButton = document.getElementById('signUp')
+        const signInButton = document.getElementById('signIn')
+        const container = document.getElementById('container')
+
+        signUpButton.addEventListener('click', () => {
+            container.classList.add('right-panel-active')
+        });
+
+        signInButton.addEventListener('click', () => {
+            container.classList.remove('right-panel-active')
+        });
+
+    }
+
+
